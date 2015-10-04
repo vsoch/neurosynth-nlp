30fbe911f035dabbde4601bfd00c99948d74d5bd
vsoch <vsochat@stanford.edu>
2015-10-03 21:54:53 -0700

    updating extractor to not query cognitive atlas

--- udf/ext_has_related_concept.py
+++ udf/ext_has_related_concept.py
@@ -8,6 +8,7 @@ APP_HOME = os.environ['APP_HOME']
 from cognitiveatlas.api import get_concept
 from nlp import do_stem, find_phrases, stem_phrases
 import pandas
+import pickle
 import numpy
 import sys
 import re
@@ -15,32 +16,16 @@ import re
 ARR_DELIM = '~^~'
 
 # Retrieve concepts from the cognitive atlas
-concepts = get_concept()
-concept_names = concepts.pandas["name"].tolist()
-concept_ids = concepts.pandas["id"].tolist()
-vocab_stemmed = stem_phrases(concept_names)
+input_pickle = "/home/02092/vsochat/SCRIPT/deepdive/neurosynth-nlp/slurm/concept_relations.pkl"
+concept_pickle = pickle.load(open(input_pickle,"rb"))
 
-# First generate a matrix of relationships, put a 1 for is-a
-pairs = set()            # pairs of concepts
-related_concepts = set() # concepts that are related to something
-
-relationdf = pandas.DataFrame(0,index=vocab_stemmed,columns=vocab_stemmed)
-for c in range(0,len(concepts.json)):
-    concept = concepts.json[c]
-    cid = concept["id"]
-    if "relationships" in concept:
-        for relation in concept["relationships"]:
-            if relation["relationship"] == "kind of":
-                # We have assertions for undefined concepts, so we need to check
-                if cid in concept_ids and relation["id"] in concept_ids:
-                    stem1 = stem_phrases([concept["name"]])[0]
-                    relation_concept = get_concept(id=relation["id"])
-                    stem2 = stem_phrases([relation_concept.json[0]["name"]])[0]
-                    relationdf.loc[stem1,stem2] = 1
-                    relationdf.loc[stem2,stem1] = 1
-                    pairs.add((stem1,stem2))
-                    related_concepts.add(stem1)
-                    related_concepts.add(stem2)
+# Retrieve concepts from the cognitive atlas
+concept_names = concept_pickle["concept_names"]
+concept_ids = concept_pickle["concept_ids"]
+vocab_stemmed = concept_pickle["vocab_stemmed"]
+pairs = concept_pickle["pairs"]            # pairs of concepts
+related_concepts = concept_pickle["related_concepts"]
+relationdf = concept_pickle["relation_df"]
 
 # For each input tuple
 for row in sys.stdin:
